04e95d1d5d28f5ced420f422cddc31b53ffe6ae0
crockpotveggies <justinlong@outlook.com>
2018-07-21 10:17:23 -0700

    Add download script.

---
+++ downloader.js
@@ -0,0 +1,102 @@
+var fs = require('fs')
+var request = require('request')
+var syncRequest = require('sync-request')
+var args = require('args')
+
+// command line args
+args
+	.option('size', 'The number of concurrent file downloads', 4)
+	.option('from', 'Cursor to a specific image in results', 0)
+	.option('output', 'Location to put images', './data')
+
+const flags = args.parse(process.argv)
+const outputImgDir = flags.output+'/images'
+
+// setup
+if(!fs.existsSync(flags.output)) {
+	fs.mkdirSync(flags.output)
+}
+if(!fs.existsSync(outputImgDir)) {
+	fs.mkdirSync(outputImgDir)
+}
+
+
+// the nasa internal API
+var apiSearch = function(size, from) {
+	var baseUrl = 'https://www.nasa.gov/api/2/ubernode/_search?size={{size}}&from={{from}}&sort=promo-date-time%3Adesc&q=((ubernode-type%3Aimage)%20AND%20(missions%3A3451))'
+	baseUrl = baseUrl.replace('{{size}}', size)
+	baseUrl = baseUrl.replace('{{from}}', from)
+	return baseUrl
+}
+
+var apiStatic = function(imageUri) {
+	var staticDomain = 'https://www.nasa.gov/sites/default/files/'
+	imageUri = imageUri.replace('public://', staticDomain)
+	return imageUri
+}
+
+// download image
+var downloadImageToUrl = (url, filename, callback) => {
+
+    var client = http;
+    if (url.toString().indexOf("https") === 0){
+      client = https;
+     }
+
+    client.request(url, function(response) {                                        
+      var data = new Stream();                                                    
+
+      response.on('data', function(chunk) {                                       
+         data.push(chunk);                                                         
+      });                                                                         
+
+      response.on('end', function() {                                             
+         fs.writeFileSync(filename, data.read());                               
+      });                                                                         
+   }).end();
+};
+
+
+// callback for recursive fetching
+var processAndFetch = function (body) {
+	// parse and iterate
+	let response = JSON.parse(body)
+	response.hits.hits.forEach(function(hit) {
+		var fileExt = hit._source['master-image'].uri.split(".")
+		fileExt = fileExt[fileExt.length-1]
+
+		// write image file
+		console.log(apiStatic(hit._source['master-image'].uri))
+		let file = syncRequest('GET', apiStatic(hit._source['master-image'].uri))
+		let outputFile = outputImgDir+'/'+hit._source['master-image'].id+'.'+fileExt
+
+		fs.writeFileSync(outputFile, file.getBody(), 'binary', function(err) {
+			throw new Error('Failed to write file to '+outputFile)
+		})
+
+		let metaData = hit._source['master-image'].id+'|'+hit._source.uri+'|'+hit._source.title+'|'+hit._source['master-image'].height+'|'+hit._source['master-image'].width
+		fs.appendFile(flags.output+'/images.csv', metaData, function (err) {
+		  if (err) throw err;
+		});
+	})
+
+  cursor += flags.size
+  download()
+}
+
+var download = function() {
+	let response = syncRequest('GET', apiSearch(flags.size, cursor))
+	
+	if(response.statusCode > 300) {
+		console.log('Download failed at image ', cursor)
+		console.log('BODY ', response.getBody())
+		throw new Error('Download failed')
+	}
+	
+	processAndFetch(response.getBody())
+}
+
+// download images
+console.log('Starting download...')
+var cursor = flags.from
+download()
